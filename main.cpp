35a247f7a41780739e589559dcdd8c56eef1a7da
Кирилл Миронов <k.mironov03@gmail.com>
2025-01-13 09:00:39 +0300

    main_template

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef vector<ll> vll;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+}
