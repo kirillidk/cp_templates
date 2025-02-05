f42e08cab2bee3896bb9cf14ef8e34a782a6c0c2
Kirill Mironov <k.mironov03@gmail.com>
2025-02-06 00:06:03 +0300

    main_template update

--- main.cpp
+++ main.cpp
@@ -3,13 +3,42 @@
 using namespace std;
 
 #define endl '\n'
+#define space ' '
 #define pb push_back
 #define all(x) (x).begin(), (x).end()
 
 typedef long long ll;
+typedef pair<ll, ll> pll;
 typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<vll> vvll;
+
+template <typename T>
+void print_single(T& arr) {
+    for (const auto& el : arr) {
+        cout << el << space;
+    }
+    cout << endl;
+}
+
+template <typename T>
+void print_pair(T& arr) {
+    for (const auto& el : arr) {
+        cout << el.first << space << el.second << endl;
+    }
+    cout << endl;
+}
+
+void solve() {}
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll t = 1;
+    // cin >> t;
+
+    while (t--) {
+        solve();
+    }
 }
